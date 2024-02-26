4f87dc78ff4b8f2da193f65f72db5db08cce3ad8
Yendersson <yendersoncolmenares67@gmail.com>
2024-02-26 10:51:55 -0300

    remove made by

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,9 +6,11 @@ const Footer = () => {
             <div>
                 Todos los derechos reservados &copy;2024  <img src={logo} alt="" />
             </div>
+            {/*
             <div>
                 Page made by: <a href="https://yendersite.netlify.app/">Yenderson Colmenares</a>
             </div>
+            */}
         </footer>
     )
 }
